c79ad45f0f1394fb5de839cdaaace59d57530613
halfbackchampion <briantqle@gmail.com>
2023-04-09 22:47:30 -0500

    added required attribute grade and combine stats to 1 position and that is displayed for each position. Still need to update those combine stats to match each position and to add in the confidential summary

--- src/components/Position.js
+++ src/components/Position.js
@@ -1,10 +1,33 @@
 const Position = (props) => {
   const p = props.position
+  const criteria = p.criteria
 
   return(
     <div>
       <h1>{p.name}</h1>
-      <img src = {p.imgSrc} alt = {p.name}/>
+      <p>For a {p.abbrev} to get selected for the draft board, he must pass <u>ALL</u> of the following criteria: </p>
+      {criteria.attributes && criteria.attributes.map(attribute => 
+        <ul>
+          <li key = {attribute.id}>
+            <div><b>[{attribute.score}]</b>: {attribute.description}</div>
+            {attribute.eligibleAttributes && attribute.eligibleAttributes.map(attribute => 
+              <ul>
+                <li key = {attribute}>
+                  {attribute}
+                </li>
+              </ul>
+            )}
+          </li>
+        </ul>
+      )}
+      {criteria.combineStats && criteria.combineStats.map(combine => 
+        <ul>
+          <li key = {combine.id}>
+            <div><b>[{combine.score}]</b>: {combine.type}</div>
+          </li>
+        </ul>
+      )}
+      <img src = {p.imgSrc} alt = {p.name} width = "256" height = "256"/>
     </div>
   )
 }
